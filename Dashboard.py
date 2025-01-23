a70d90d9d9fe9ca52d976633438670cfcaad0cac
VegetablePC <113391828+VegetablePC@users.noreply.github.com>
2025-01-23 04:46:16 -0500

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -31,3 +31,4 @@ st.markdown("<h1 style='text-align: center; color: white; '>Losses During Clear 
 fig2.update_yaxes(range=[0, 3500]) # Sets range to 3500 to match fig1
 fig2.update_layout(yaxis_title="Total Loss in Dollars") # Updates the Y Axis to say "Total Loss in Dollars" instead of default column title
 st.plotly_chart(fig2)
+st.write("Total loss during clear weather: ", total2)
