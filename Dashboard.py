9c70b3b89423e3894318a6f72b667e1462da66d4
VegetablePC <113391828+VegetablePC@users.noreply.github.com>
2025-01-23 03:45:26 -0500

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -6,6 +6,7 @@ import requests as rq
 # Reads both CSV sheets for data
 data1 = pd.read_csv("crimestormQ.csv")
 data2 = pd.read_csv("crimenostormQ.csv")
+desc = print(Description.txt)
 
 
 #Plots our initial lines by defining fig1 and fig2 as plotly lines
@@ -21,3 +22,5 @@ fig2.update_yaxes(range=[0, 3500]) # Sets range to 3500 to match fig1
 fig2.update_layout(title_text="Claims During Clear Weather", title_x=0.4) # Titles the graph and moves the title closer to center
 fig2.update_layout(yaxis_title="Total Loss in Dollars") # Updates the Y Axis to say "Total Loss in Dollars" instead of default column title
 st.plotly_chart(fig2)
+
+print("  These two graphs display a difference in loss statistics between periods of storm activity and clear weather. As we can see, the amount of loss in dollars is clearly higher during periods of storm activity vs when clear weather occurs."
