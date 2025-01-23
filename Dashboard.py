3dc800b33d131d3a26469cea820b34a29da35241
VegetablePC <113391828+VegetablePC@users.noreply.github.com>
2025-01-23 03:37:30 -0500

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -15,7 +15,7 @@ fig2 = px.line(data2, x="Date", y="Loss")
 st.subheader("Claims During Stormy Weather")
 
 fig1.update_yaxes(range=[0, 3500]) # Sets range to 3500 to match fig2
-fig1.update_layout(title_text="Claims During Stormy Weather", title_x=0.3)
+fig1.update_layout(title_text="Claims During Stormy Weather", title_x=0.35)
 fig1.update_layout(yaxis_title="Total Loss in Dollars") # Updates the Y Axis to say "Total Loss in Dollars" instead of default column title
 st.plotly_chart(fig1)
 
@@ -23,6 +23,6 @@ st.plotly_chart(fig1)
 st.subheader("Claims During Clear Weather")
 
 fig2.update_yaxes(range=[0, 3500]) # Sets range to 3500 to match fig1
-fig2.update_layout(title_text="Claims During Clear Weather", title_x=0.3)
+fig2.update_layout(title_text="Claims During Clear Weather", title_x=0.35)
 fig2.update_layout(yaxis_title="Total Loss in Dollars") # Updates the Y Axis to say "Total Loss in Dollars" instead of default column title
 st.plotly_chart(fig2)
