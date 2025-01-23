17a2bb1f8cc280922aa5e1d52650f3798e60e18c
VegetablePC <113391828+VegetablePC@users.noreply.github.com>
2025-01-23 03:56:57 -0500

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -13,15 +13,15 @@ fig1 = px.line(data1, x="Date", y="Loss")
 fig2 = px.line(data2, x="Date", y="Loss")
 
 
-st.markdown("<h1 style='text-align: center; color: grey; '>Claims During Stormy Weather</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: white; '>Claims During Stormy Weather</h1>", unsafe_allow_html=True)
 fig1.update_yaxes(range=[0, 3500]) # Sets range to 3500 to match fig2
 fig1.update_layout(yaxis_title="Total Loss in Dollars") # Updates the Y Axis to say "Total Loss in Dollars" instead of default column title
 st.plotly_chart(fig1)
 
-st.markdown("<h1 style='text-align: center; color: grey; '>Claims During Clear Weather</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: white; '>Claims During Clear Weather</h1>", unsafe_allow_html=True)
 fig2.update_yaxes(range=[0, 3500]) # Sets range to 3500 to match fig1
 fig2.update_layout(yaxis_title="Total Loss in Dollars") # Updates the Y Axis to say "Total Loss in Dollars" instead of default column title
 st.plotly_chart(fig2)
 
-st.markdown("<h1 style='text-align: center; color: grey; '>Description</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: white; '>Description</h1>", unsafe_allow_html=True)
 st.write(desc.read())
