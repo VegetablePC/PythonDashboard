9af18cc7f4b223f492cdbcc221ab619b333ca6d6
VegetablePC <113391828+VegetablePC@users.noreply.github.com>
2025-01-23 04:20:15 -0500

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -15,12 +15,12 @@ fig2 = px.line(data2, x="Date", y="Loss")
 st.markdown("<h1 style='text-align: center; color: white; '>Losses in Dollars in Stormy and Clear Weather</h1>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center; '>These two graphs display a difference in loss statistics between periods of storm activity and clear weather. As we can see, the amount of loss in dollars is clearly higher during periods of storm activity vs when clear weather occurs</p>", unsafe_allow_html=True)
 
-st.markdown("<h1 style='text-align: center; color: white; '>Claims During Stormy Weather</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: white; '>Losses During Stormy Weather</h1>", unsafe_allow_html=True)
 fig1.update_yaxes(range=[0, 3500]) # Sets range to 3500 to match fig2
 fig1.update_layout(yaxis_title="Total Loss in Dollars") # Updates the Y Axis to say "Total Loss in Dollars" instead of default column title
 st.plotly_chart(fig1)
 
-st.markdown("<h1 style='text-align: center; color: white; '>Claims During Clear Weather</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: white; '>Losses During Clear Weather</h1>", unsafe_allow_html=True)
 fig2.update_yaxes(range=[0, 3500]) # Sets range to 3500 to match fig1
 fig2.update_layout(yaxis_title="Total Loss in Dollars") # Updates the Y Axis to say "Total Loss in Dollars" instead of default column title
 st.plotly_chart(fig2)
