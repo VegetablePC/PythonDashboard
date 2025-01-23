a01c24e3f7cd1f86222138a16d9f1e83407b7ec1
VegetablePC <113391828+VegetablePC@users.noreply.github.com>
2025-01-23 03:34:36 -0500

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -6,20 +6,20 @@ import plotly.express as px
 data1 = pd.read_csv("crimestormQ.csv")
 data2 = pd.read_csv("crimenostormQ.csv")
 
+
 #Plots our initial lines by defining fig1 and fig2 as plotly lines
 fig1 = px.line(data1, x="Date", y="Loss") 
 fig2 = px.line(data2, x="Date", y="Loss")
 
 # Title for graph 1
+st.subheader("Claims During Stormy Weather", title_x=0.5)
+
 fig1.update_yaxes(range=[0, 3500]) # Sets range to 3500 to match fig2
-fig1.update_layout(title={
-  "Claims during Storms",
-  'xanchor': 'center'})
 fig1.update_layout(yaxis_title="Total Loss in Dollars") # Updates the Y Axis to say "Total Loss in Dollars" instead of default column title
 st.plotly_chart(fig1)
 
 # Title for graph 2
-st.subheader("Claims During Clear Weather")
+st.subheader("Claims During Clear Weather", title_x=0.5)
 
 fig2.update_yaxes(range=[0, 3500]) # Sets range to 3500 to match fig1
 fig2.update_layout(yaxis_title="Total Loss in Dollars") # Updates the Y Axis to say "Total Loss in Dollars" instead of default column title
