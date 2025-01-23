d1ee86f8d87061d9f104d4e2222c22bfdf1d4dd1
VegetablePC <113391828+VegetablePC@users.noreply.github.com>
2025-01-23 03:58:21 -0500

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -24,4 +24,4 @@ fig2.update_layout(yaxis_title="Total Loss in Dollars") # Updates the Y Axis to 
 st.plotly_chart(fig2)
 
 st.markdown("<h1 style='text-align: center; color: white; '>Description</h1>", unsafe_allow_html=True)
-st.write(textwrap.indent((desc.read())
+st.write(textwrap.indent((desc.read()))
