c1fb9681ce83d901c2029073fe93d7e8b3dbe0ca
VegetablePC <113391828+VegetablePC@users.noreply.github.com>
2025-01-23 03:51:15 -0500

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -22,4 +22,5 @@ fig2.update_layout(title_text="Claims During Clear Weather", title_x=0.4) # Titl
 fig2.update_layout(yaxis_title="Total Loss in Dollars") # Updates the Y Axis to say "Total Loss in Dollars" instead of default column title
 st.plotly_chart(fig2)
 
+desc.update_layout(title_text="Description", title_x=0.4)
 st.write(desc.read())
