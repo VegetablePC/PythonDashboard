244eff46b77fde5bf8f44069b22e5ce1fbffe032
VegetablePC <113391828+VegetablePC@users.noreply.github.com>
2025-01-23 03:04:16 -0500

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -5,7 +5,7 @@ import plotly.express as px
 data1 = pd.read_csv("crimestormQ.csv")
 data2 = pd.read_csv("crimenostormQ.csv")
 
-fig1 = px.line(data1, x="Date", y="Loss")
+fig1 = px.line(data1, x="Date", y="Loss (In Dollars)")
 fig2 = px.line(data2, x="Date", y="Loss")
 
 st.subheader("Claims During Storms")
